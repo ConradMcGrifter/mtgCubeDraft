6132ee70527227bbdb7a84266fd0c578ae96a553
skf <79630413+ConradMcGrifter@users.noreply.github.com>
2021-05-26 00:31:10 -0400

    added card img on hover of removed cards

--- navBar.js
+++ navBar.js
@@ -1,14 +1,17 @@
 let hamburger = document.getElementById("hamburger");
 let navMenu = document.querySelector(".removed");
 let pageTint = document.querySelector(".page-tint");
+// let removedWrap = document.querySelector(".removed-wrap");
 
 function revealMenu() {
     // document.querySelector("body").classList.toggle("display");
     navMenu.classList.toggle("display");
     pageTint.classList.toggle("display");
+    // removedWrap.classList.toggle("display");
 }
 
 pageTint.addEventListener("click", () => {
     pageTint.classList.toggle("display");
     navMenu.classList.toggle("display");
+    // removedWrap.classList.toggle("display");
 });
